bd8d972575aff7695da2763601c0826a80d425bb
Yoshida Shin <wbcchsyn@gmail.com>
2023-09-26 07:41:30 +0900

    Pass builder/test04

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -21,6 +21,7 @@ fn do_derive(input: TokenStream) -> Result<TokenStream2, syn::Error> {
     let dst_struct = dst_struct(&dst_struct_name, src_fields.clone());
     let src_builder_method = src_builder_method(&dst_struct_name, src_fields.clone());
     let setter_methods = src_fields.clone().map(setter_method);
+    let dst_build_method = dst_build_method(src_struct_name, src_fields.clone());
 
     Ok(quote! {
         #dst_struct
@@ -31,6 +32,8 @@ fn do_derive(input: TokenStream) -> Result<TokenStream2, syn::Error> {
 
         impl #dst_struct_name {
             #(#setter_methods)*
+
+            #dst_build_method
         }
     })
 }
@@ -104,6 +107,38 @@ fn setter_method(src_field: &syn::Field) -> TokenStream2 {
         pub fn #name(&mut self, #name: #ty) -> &mut Self {
             self.#name = Some(#name);
             self
+        }
+    }
+}
+
+fn dst_build_method<'a, T>(src_name: &syn::Ident, src_fields: T) -> TokenStream2
+where
+    T: Iterator<Item = &'a syn::Field> + Clone,
+{
+    let built_check = src_fields.clone().map(|field|{
+        let name = field.ident.as_ref().unwrap();
+        quote! {
+            if self.#name.is_none() {
+                let msg = format!("Field {} is not set yet.", stringify!(#name));
+                return Err(msg.into());
+            }
+        }
+    });
+
+    let built_fields = src_fields.clone().map(|field| {
+        let name = field.ident.as_ref().unwrap();
+        quote! {
+            #name: self.#name.take().unwrap(),
+        }
+    });
+
+    quote! {
+        pub fn build(&mut self) -> ::std::result::Result<#src_name, ::std::boxed::Box<dyn ::std::error::Error>> {
+            #(#built_check)*
+
+            ::std::result::Result::Ok(#src_name {
+                #(#built_fields)*
+            })
         }
     }
 }
