cda9f363de2a08c568af66eb8790ce9a20c72c83
Yoshida Shin <wbcchsyn@gmail.com>
2023-09-26 18:56:02 +0900

    Pass sorted/test02

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,31 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
 use syn;
-use quote::ToTokens;
 
 #[proc_macro_attribute]
-pub fn sorted(_: TokenStream, input: TokenStream) -> TokenStream {
+pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args: TokenStream2 = args.into();
     let item = syn::parse_macro_input!(input as syn::Item);
-    item.into_token_stream().into()
+
+    let err = match do_sorted(&args, &item) {
+        Ok(()) => TokenStream2::new(),
+        Err(err) => err.to_compile_error(),
+    };
+
+    quote! {
+        #item
+        #err
+    }
+    .into()
+}
+
+fn do_sorted(args: &TokenStream2, item: &syn::Item) -> syn::Result<()> {
+    match item {
+        syn::Item::Enum(_) => Ok(()),
+        _ => Err(syn::Error::new_spanned(
+            args,
+            "expected enum or match expression",
+        )),
+    }
 }
