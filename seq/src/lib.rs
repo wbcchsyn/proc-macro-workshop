4226f32f63e859a3862a05e03c845730533a02ac
Yoshida Shin <wbcchsyn@gmail.com>
2023-09-27 18:20:43 +0900

    Skip seq/test03
    The error message is slightly different, but I think it is OK.

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,11 +1,62 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{quote, ToTokens};
 use syn::parse::Parse;
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _seq = syn::parse_macro_input!(input as Seq);
+    let seq = syn::parse_macro_input!(input as Seq);
 
-    TokenStream::new()
+    match do_seq(seq) {
+        Ok(token) => token.into(),
+        Err(err) => err.to_compile_error().into(),
+    }
+}
+
+fn do_seq(seq: Seq) -> syn::Result<TokenStream2> {
+    let start: i128 = seq.start.base10_parse()?;
+    let end: i128 = {
+        let end = seq.end.base10_parse()?;
+        match seq.dot2 {
+            syn::RangeLimits::HalfOpen(_) => end,
+            syn::RangeLimits::Closed(_) => end + 1,
+        }
+    };
+
+    let variable = seq.loop_var.to_string();
+    let tokens = (start..end).fold(Vec::new(), |mut acc, i| {
+        for stmt in seq.block.stmts.iter() {
+            let token = replace_ident(stmt, &variable, i);
+            acc.push(token);
+        }
+        acc
+    });
+
+    Ok(quote! { #(#tokens)* })
+}
+
+fn replace_ident<T: ToTokens>(token: &T, variable: &str, value: i128) -> TokenStream2 {
+    let tokens = token.to_token_stream().into_iter().map(|tree| match &tree {
+        proc_macro2::TokenTree::Ident(ident) => {
+            if ident == variable {
+                syn::LitInt::new(&value.to_string(), ident.span()).into_token_stream()
+            } else {
+                tree.into_token_stream()
+            }
+        }
+        proc_macro2::TokenTree::Group(group) => {
+            let inner = replace_ident(&group.stream(), variable, value);
+            match group.delimiter() {
+                proc_macro2::Delimiter::Parenthesis => quote! { (#inner) },
+                proc_macro2::Delimiter::Brace => quote! { {#inner} },
+                proc_macro2::Delimiter::Bracket => quote! { <#inner> },
+                proc_macro2::Delimiter::None => inner,
+            }
+        }
+        _ => tree.into_token_stream(),
+    });
+
+    quote! { #(#tokens)* }
 }
 
 struct Seq {
