bbc963f5e9d9b48991278f6a99624015719527cd
Yoshida Shin <wbcchsyn@gmail.com>
2023-09-27 13:23:22 +0900

    Pass seq/test01

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,31 @@
 use proc_macro::TokenStream;
+use syn::parse::Parse;
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _seq = syn::parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    TokenStream::new()
+}
+
+struct Seq {
+    loop_var: syn::Ident,
+    _in: syn::Token![in],
+    start: syn::LitInt,
+    dot2: syn::RangeLimits,
+    end: syn::LitInt,
+    block: Box<syn::Block>,
+}
+
+impl Parse for Seq {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            loop_var: input.parse()?,
+            _in: input.parse()?,
+            start: input.parse()?,
+            dot2: input.parse()?,
+            end: input.parse()?,
+            block: input.parse()?,
+        })
+    }
 }
